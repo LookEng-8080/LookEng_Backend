5bd2c12dcabf51c5486465792a7e140ce82e23ec
thdgus0307 <bsh0374@gmail.com>
2026-04-04 11:15:10 +0900

    Project Initialization

---
+++ src/main/java/com/sw8080/lookeng/LookEngApplication.java
@@ -0,0 +1,13 @@
+package com.sw8080.lookeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookEngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookEngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw8080/lookeng/LookEngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw8080.lookeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookEngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
